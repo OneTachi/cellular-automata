f22f93f7deaa0c88e12809c5e05db39b46de8b26
Snazzy0895 <155108461+Snazzy0895@users.noreply.github.com>
2023-12-29 15:18:05 -0500

    Cell Class Modification
    Cell Class Containing:
    - Constructor
    - Getter and Setter for value

---
+++ cell.hh
@@ -0,0 +1,36 @@
+
+
+#ifndef CELL_HH
+#define CELL_HH
+
+#include <vector>
+
+using namespace std;
+
+// Cell class - they should contain some value, 0 for floor, 1 for rock, 2 for wall (might need to change in future for rules to work), function to get the value of neighborhood (getneighborhood?)
+
+class Cell
+{
+public:
+	Cell(int);
+	void setCellValue(int);
+	int getCellValue();
+	void print();
+	
+private:
+	int value;
+}
+//Constructor
+Cell:Cell(int valueGiven)
+{
+	value = valueGiven;
+}
+//Getter and Setter for value
+Cell:setCellValue(int newVal)
+{
+	value = newVal;
+}
+Cell:getCellValue()
+{
+	return value;
+}
