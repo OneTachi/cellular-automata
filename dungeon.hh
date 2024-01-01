cf3ff01869972c2951336f1b053a22ea8459d0eb
Snazzy0895 <155108461+Snazzy0895@users.noreply.github.com>
2023-12-31 22:02:28 -0500

    Added dungeon constructor and getNeighborhood
    Constructor:
    - Fills every slot with a cell at value 0
    
    getNeighborhood:
    - Takes a row and coloumn index of a cell
    - Checks the surrounding eight cells values, and added them together
    - returns this value

--- dungeon.hh
+++ dungeon.hh
@@ -8,6 +8,9 @@
 
 #include <vector>
 
+//The value each cell starts with
+int startingCellValue = 0;
+
 using namespace std; 
 class Dungeon
 {
@@ -15,6 +18,7 @@ public:
 	Dungeon(int, int);
 	void setTile(pair<int, int>, int);
 	int getTile(pair<int, int>);
+	int getNeighborhood(int, int);
 	void print();
 	
 private:
@@ -36,13 +40,84 @@ Dungeon::Dungeon(int rows, int cols)
 	dungeon = vector<vector<int>>(num_rows, vector<int>(num_cols));
 
 	// Add cells into each index
-	
+	for(int i=0, i<num_rows, i++)
+	{
+		for(int j=0, j<num_cols, j++)
+		{
+			Cell startCell = new Cell(startingCellValue);
+			dungeon[i][j].push_back(startCell);
+		}
+	}
 
 	// Put references in for each cell/neighborhood
 }
 
 void Dungeon::setTile(pair<int, int> coords, int value) { dungeon[coords.first][coords.second] = value; }
 int Dungeon::getTile(pair<int, int> coords) { return dungeon[coords.first][coords.second]; }
+
+int getNeighborhood(int rowIndex, int colIndex)
+{
+	int neighborhoodValue = 0;
+	Cell temp = getTile(rowIndex, colIndex);
+	bool topValid = false;
+	bool bottomValid = false;
+	
+	//Cases where index of cell is at edge of dungeon
+	if(rowIndex > 0)
+	{
+		//Gets node directly above the starting node
+		temp = getTile(rowIndex-1, colIndex);
+		neighborhoodValue += temp->getCellValue();
+		//States the top is possibly valid
+		topValid = true;
+	}
+	if(rowIndex < num_rows)
+	{
+		//Gets node directly above the starting node
+		temp = getTile(rowIndex+1, colIndex);
+		neighborhoodValue += temp->getCellValue();
+		//Says bottom is possibly valid
+		bottomValid = true;
+	}
+	if(colIndex > 0)
+	{
+		//Gets node directly left of the starting node
+		temp = getTile(rowIndex, colIndex-1);
+		neighborhoodValue += temp->getCellValue();
+		//Checks for the top left node 
+		if(topValid)
+		{
+			temp = getTile(rowIndex-1, colIndex-1);
+			neighborhoodValue += temp->getCellValue();
+		}
+		//Checks for the bottom left node 
+		if(bottomValid)
+		{
+			temp = getTile(rowIndex+1, colIndex-1);
+			neighborhoodValue += temp->getCellValue();
+		}
+	}
+	if(rowIndex < num_cols)
+	{
+		//Gets node directly right of the starting node
+		temp = getTile(rowIndex, colIndex+1);
+		neighborhoodValue += temp->getCellValue();
+		//Checks for the top right node 
+		if(topValid)
+		{
+			temp = getTile(rowIndex-1, colIndex+1);
+			neighborhoodValue += temp->getCellValue();
+		}
+		//Checks for the bottom right node 
+		if(bottomValid)
+		{
+			temp = getTile(rowIndex+1, colIndex+1);
+			neighborhoodValue += temp->getCellValue();
+		}
+	}
+	
+	return neighborhoodValue;
+}
 
 //TODO 
 // print() -- Sidd
