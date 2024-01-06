029d8ac83d2e8caecfa546a518fbaa89a9eb2ac4
sidd <n2verz@gmail.com>
2024-01-06 12:55:53 -0500

    update ruleset and distinguish generations

--- dungeon.hh
+++ dungeon.hh
@@ -11,26 +11,33 @@
 //The value each cell starts with
 const int startingCellValue = 0;
 
-const int FLOOR = 0;
-const int ROCK = 1;
-const int WALL = 2;
+// We will keep this simple, a floor and a wall. 1 & 0 will allow us to easily calculate the moore's neighborhood and determine # of floors/walls
+const int FLOOR = 1; 
+const int WALL = 0;
 
 using namespace std; 
 class Dungeon
 {
 public:	
 	Dungeon(int, int);
+	
 	void set_tile(pair<int, int>, int);
+	void set_temp_tile(pair<int, int>, int); // Set tile for temporary grid
 	int get_tile(pair<int, int>);
-	int get_neighborhood(int, int, int);
+	int get_temp_tile(pair<int, int>);
+
+	int get_neighborhood(int, int, int); // Get Moore Neighborhood
 	void print();
 	void apply_rule(pair<int, int>);
+	void calculate_generation();
 	
 private:
 	vector<vector<int>> dungeon; // Row then column for access
+	vector<vector<int>> temp_dungeon; // Our temporary grid for each generation
 	int num_rows;
 	int num_cols;
-	int time = 0;
+	int time = 0; // The current generation we are on
+	int generations = 0; // Number of generations we would like to go up to
 };
 
 Dungeon::Dungeon(int rows, int cols)
@@ -44,23 +51,14 @@ Dungeon::Dungeon(int rows, int cols)
 	
 	// Set dungeon size
 	dungeon = vector<vector<int>>(num_rows, vector<int>(num_cols));
-
-	// Add cells into each index
-	/*
-	for(int i=0; i<num_rows; i++)
-	{
-		for(int j=0; j<num_cols; j++)
-		{
-			dungeon[i][j] = startingCellValue;
-		}
-	}
-	*/
-
-	// Put references in for each cell/neighborhood
 }
 
+// Getters and Setters
 void Dungeon::set_tile(pair<int, int> coords, int value) { dungeon[coords.first][coords.second] = value; }
+void Dungeon::set_temp_tile(pair<int, int> coords, int value) { temp_dungeon[coords.first][coords.second] = value; }
 int Dungeon::get_tile(pair<int, int> coords) { return dungeon[coords.first][coords.second]; }
+int Dungeon::get_temp_tile(pair<int, int> coords) { return temp_dungeon[coords.first][coords.second]; }
+
 //If val = 0 return isNextToFloor, else return neighborhoodTotalValue
 int Dungeon::get_neighborhood(int val, int rowIndex, int colIndex) 
 {
@@ -162,16 +160,32 @@ int Dungeon::get_neighborhood(int val, int rowIndex, int colIndex)
 	return isNextToFloor;
 }
 
+/**
+ * This is the ruleset we will apply to the current tile. It makes its changes onto a temporary grid
+ * Things to consider forward: Do we want to use time for our rules? 
+ */
 void Dungeon::apply_rule(pair<int, int> coords)
 {
 	int neighborhood = get_neighborhood(coords.first, coords.second);
 	
-	// If the neighborhood is greater than generation, set this tile to rock. Otherwise, set this tile to floor
-	if (neighborhood >= time) { set_tile(coords, ROCK); }
-	else { set_tile(coords, FLOOR); }	
+	// If neighborhood has 4 or more walls, the current tile is a floor. Otherwise, it will be a wall.	
+	if (neighborhood >= 4) { set_temp_tile(coords, FLOOR); }
+	else { set_temp_tile(coords, WALL); }	
+}
 
-	// If rock cell and floor nearby, turn this into wall? Or do we turn floor into wall?
-	// TODO
+void Dungeon::calculate_generation()
+{
+	// Iterate through grid one by one and apply rule for temp grid
+	for (int row = 0; row < num_rows; row++)
+	{
+		for (int col = 0; col < num_cols; col++)
+		{
+			apply_rule(pair<int, int>(row, col));
+		}
+	}
+
+	// Copy temp grid to dungeon grid TODO
+	
 }
 
 
